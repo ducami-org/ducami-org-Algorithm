8593b3d996701a68f9b983e4668b34a19dbd91a9
dgsw62 <yongjin@dgsw62ui-MacBookAir.local>
2024-04-22 18:06:18 +0900

    baekjoon 10807

---
+++ backjoon.py
@@ -0,0 +1,11 @@
+#나의 답안
+a = int(input())
+b = list(map(int,input().split()))
+c = int(input())
+print(b.count(c))
+#다른 답안
+N = int(input())
+N_list = list(map(int,input().split()))
+v = int(input())
+
+print(N_list.count(v))
