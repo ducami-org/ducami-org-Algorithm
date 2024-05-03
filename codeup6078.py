2d3e8a1dac0b63eca43fef875ede398b27c070fb
donggyengho <ukat7@gmail.com>
2024-05-03 14:08:32 +0900

    codeup 78, 79, 80, 81, 82 번 문제 동경호

---
+++ codeup6078.py
@@ -0,0 +1,6 @@
+a = input()
+while True:
+    print(a)
+    if a == 'q':
+        break
+    a = input()
---
+++ codeup6079.py
@@ -0,0 +1,11 @@
+a = int(input())
+n = 0
+c = 0
+b = 0
+while True:
+    if c >= a:
+        print(b)
+        break
+    n += 1 
+    c += n
+    b += 1
