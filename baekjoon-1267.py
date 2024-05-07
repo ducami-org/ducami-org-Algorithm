ea5f51c29737f8f84630b85ce8987f1608bb799d
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-05-07 16:59:03 +0900

    baekjoon-1267 모음의 개수

---
+++ baekjoon-1267.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = list(map(int,input().split()))
+c =0
+d=0
+for i in range(a):
+    c+= (b[i]//30+1)*10
+    d +=(b[i]//60+1)*15
+    
+if(c>d):
+    print('M', d)
+elif(c<d):
+    print('Y', c)
+else:
+    print('Y','M', c)
