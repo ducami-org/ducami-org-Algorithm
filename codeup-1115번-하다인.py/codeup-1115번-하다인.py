29488e0eece89ce5877559d1c10f3fad36cf2321
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-05-07 16:39:35 +0900

    codeup-6083번-하다인

---
+++ codeup-1115번-하다인.py/codeup-1115번-하다인.py
@@ -0,0 +1,11 @@
+date1 = [1, 2, 3, 4, 5]
+date2 = date1.copy()
+
+print("date1은 ", date1)
+print("date2은 ", date2)
+
+date1[0] = 10
+print("date1 변경 후")
+
+print("date1은 ", date1)
+print("date2은 ", date2)
