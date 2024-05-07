c3bb5c3de3f8bf12b9b856eefff9b37973dcf0eb
dgsw23 <dgsw23@dgsw23ui-noteubug.local>
2024-05-07 20:54:45 +0900

    codeup-1161-1165

---
+++ codeup1164.py
@@ -0,0 +1,2 @@
+t_data = list(map(int, input().split()))
+print("CRASH") if min(t_data) <= 170 else print("PASS")
---
+++ codeup1165.py
@@ -0,0 +1,4 @@
+cur_time, our_score = map(int, input().split())
+for i in range(cur_time, 90, 5):
+    our_score += 1
+print(our_score)
