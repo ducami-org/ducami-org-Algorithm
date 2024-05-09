18fc3a143772234856d0fb5730f5f051f46aa044
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-05-09 17:06:55 +0900

    baekjoon 2530 인공지능 시계

--- baekjoon2530인공지능 시계.py
+++ baekjoon2530인공지능 시계.py
@@ -1,3 +1,4 @@
+#내 답안
 a,b,c = map(int,input().split())
 d = int(input())
 temp = a * 3600 + b * 60 + c + d
@@ -10,3 +11,25 @@ elif  temp // 3600 == 24:
 
 else:
     print(f"{temp//3600} {temp%3600//60} {temp%60}")
+    
+#다른 답안
+H, M, S = map(int, input().split())
+D = int(input()) 
+
+S += D % 60
+D = D // 60
+if S >= 60:
+    S -= 60
+    M += 1
+
+M += D % 60
+D = D // 60
+if M >= 60:
+    M -= 60
+    H += 1
+
+H += D % 24
+if H >= 24:
+    H -= 24
+
+print(H,M,S)
