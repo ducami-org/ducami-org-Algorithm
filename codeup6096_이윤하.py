7e941c589d8f20417f40337d286b0fe3b4752553
이윤하 <leeyoonha2015@gmail.com>
2024-05-09 18:05:09 +0900

    codeup6069번 바둑알 십자뒤집기 5.9

---
+++ codeup6096_이윤하.py
@@ -0,0 +1,23 @@
+arr=[]
+for i in range(19):
+    arr.append(list(map(int,input().split())))
+n=int(input())
+
+for i in range(n):
+    x,y=map(int,input().split())
+    for j in range(19):
+        if  arr[j][y-1]==0:
+            arr[j][y-1]=1
+        else:
+            arr[j][y-1]=0
+
+        if arr[x-1][j]==0:
+            arr[x-1][j]=1
+        else:
+            arr[x-1][j]=0
+       
+
+for i in range(19):
+    for j in range(19):
+        print(arr[i][j],end=' ')
+    print()
