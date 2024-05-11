8e0f64439a841e5aea3d224da6303705ff2628aa
tlsalsco <tlsalsco@dgsw.hs.kr>
2024-05-11 20:04:18 +0900

    codeup-1168번 나이계산1

---
+++ codeup-1168번 나이계산1.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+b = int(b)
+
+if(b == 1):
+    print(2012 - int('19' + a[0:2]) + 1)
+elif(b == 2):
+    print(2012 - int('19' + a[0:2]) + 1)
+elif(b == 3):
+    print(2012 - int('20' + a[0:2]) + 1)
+else:
+    print(2012 - int('20' + a[0:2]) + 1)
