ab22d3b78a671b1d8688b79faa38797d406b3827
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-05-11 17:25:22 +0900

    baekjoon2752 세수정렬

---
+++ baekjoon2752.py
@@ -0,0 +1,9 @@
+# 첫 번째 답안
+a = list(map(int,input().split()))
+a.sort()
+for i in range(len(a)):
+    print(a[i],end=' ')
+# 두 번째 답안
+# a = list(int,input().split())
+# for i in range(len(a)):
+#     print(min(a))
