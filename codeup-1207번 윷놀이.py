b48590b3aefb3b6aec7e5946e0427511922bcd56
tlsalsco <tlsalsco@dgsw.hs.kr>
2024-05-13 15:18:58 +0900

    codeup-1207번 윷놀이

---
+++ codeup-1207번 윷놀이.py
@@ -0,0 +1,13 @@
+data = list(map(int, input().split()))
+result = data.count(1)
+
+if result == 0: 
+    print('모')
+elif result == 1: 
+    print('도')
+elif result == 2: 
+    print('개')
+elif result == 3: 
+    print('걸')
+else: 
+    print('윷')
