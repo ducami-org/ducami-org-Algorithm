859ad246a8f5e1f2d56ab54ef178596b0cd56899
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-05-14 18:54:06 +0900

    baekjoon-23794 골뱅이 찍기 - 정사각형

---
+++ baekjoon-23794.py
@@ -0,0 +1,12 @@
+a =int(input())
+print("@",end="")
+print("@"*a,end="")
+print("@")
+for i in range(a):
+    print("@",end="")
+    print(" "*a,end="")
+    print("@")
+print("@",end="")
+print("@"*a,end="")
+print("@")
+    
