f1555aea318d5868cd6e38f8d0d520d74d032816
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-05-14 19:25:20 +0900

    codeup-1160번-하다인

---
+++ codeup-1160번-하다인.py
@@ -0,0 +1,15 @@
+a=int(input())
+if a==1:
+    print('oh my god')
+elif a==2:
+    print('enjoy')
+elif a==3:
+    print('oh my god')
+elif a==4:
+    print('enjoy')
+if a==5:
+    print('oh my god')
+elif a==6:
+    print('enjoy')
+if a==7:
+    print('oh my god')
