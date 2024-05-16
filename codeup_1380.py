0d85ff1283d264ec7e8b7dd92a171d762b1380cf
kimseonghan <imground11@gmail.com>
2024-05-16 14:15:17 +0900

    codeup_1382

--- codeup_1380.py
+++ codeup_1380.py
@@ -1,6 +1,4 @@
-k = int(input())
-for i in range(1, k+1):
-    for j in range(k, 0, -1):
-        if i<7 and j<7:
-            if i+j == k:
-                print(i, j)
+for i in range (1, 10):
+    for j in range(2, 6):
+        print(f"{j} x {i} = {'%2d' % (i*j)}\t", end='')
+    print()
