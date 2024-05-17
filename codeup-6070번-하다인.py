8e32061aceaf4117a8547d140e8b3687e3e850a8
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-05-17 20:46:24 +0900

    codeup-6070번-하다인

---
+++ codeup-6070번-하다인.py
@@ -0,0 +1,13 @@
+a=int(input())
+if a==12:
+    print('winter')
+if a==1:
+    print('winter')
+if a==2:
+    print('winter')
+elif a//3==1:
+    print('spring')
+elif a//3==2:
+    print('summer')
+elif a//3==3:
+    print('fall')
