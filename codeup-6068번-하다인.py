9dd5d2148491a552e93805cfdcb4e82fe54d64fc
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-05-17 20:48:49 +0900

    codeup-6068번-하다인

---
+++ codeup-6068번-하다인.py
@@ -0,0 +1,12 @@
+a=int(input())
+if a>=90:
+    print('A')
+else:
+    if a>=70:
+        print('B')
+    else:
+        if a>=40:
+            print('C')
+        else:
+            if a>=0:
+                print('D')
