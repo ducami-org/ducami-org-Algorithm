096473346989edf0c3c25a466e91205722f94a5d
kimseonghan <imground11@gmail.com>
2024-05-22 13:58:36 +0900

    codeup_1460

---
+++ codeup_1460.py
@@ -0,0 +1,11 @@
+a = int(input())
+sum = 0
+arr = [[0 for j in range(a)] for i in range(a)]
+for i in range(a):
+    for j in range(a):
+        sum+=1
+        arr[i][j] = sum
+for i in range(a):
+    for j in range(a):
+        print(arr[i][j], end=' ')
+    print()
