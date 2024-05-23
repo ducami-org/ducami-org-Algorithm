781ead5c2bd791ff6c8bcbcd112768f007b779b8
kimseonghan <imground11@gmail.com>
2024-05-23 11:37:46 +0900

    codeup_1465

---
+++ codeup_1465.py
@@ -0,0 +1,11 @@
+a,b = map(int, input().split())
+sum = a*b
+arr = [[0 for j in range(b)] for i in range(a)]
+for i in range(a):
+    for j in range(b):
+        arr[i][b-j-1] = sum
+        sum-=1
+for i in range(a):
+    for j in range(b):
+        print(arr[i][j], end=' ')
+    print()
