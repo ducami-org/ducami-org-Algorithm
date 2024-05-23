e9b4276f06730281866898dc5bb18da2f005e319
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-05-23 14:45:47 +0900

    baekjoon10101 상근이의 친구들

--- baekjoon5717.py
+++
@@ -1,8 +0,0 @@
-b = 0
-while b == 0:
-    a,c = map(int,input().split())
-    sum_ab = a+c
-    if a == 0 and c ==0:
-        b += 1
-    else:
-        print(sum_ab)
---
+++ baekjoon브론즈4.py
@@ -0,0 +1,58 @@
+#baekjoon10101 상근이의 친구들
+
+# b = 0
+# while b == 0:
+#     a,c = map(int,input().split())
+#     sum_ab = a+c
+#     if a == 0 and c ==0:
+#         b += 1
+#     else:
+#         print(sum_ab)
+
+#baekjoon10039 평균 점수
+# sum = 0
+# for i in range(5):
+#     a = 0
+#     a = int(input())
+#     if a%5==0 and 40 <= a:
+#         sum += a
+#     elif a%5==0 and 40 > a:
+#         sum += 40
+# print(int(sum/5))
+
+#baekjoon10808 알파벳 개수
+# a = list(input())
+# li = [int(0) for i in range(26)]
+# for i in a:
+#     li[ord(i)-97] += 1
+# for i in li:
+#     print(i,end=' ')
+
+#baekjoon5543 상근날드
+# a = []
+# b = []
+# for i in range(5):
+#     a.append(int(input()))
+    
+# sc = a[0]+a[3] - 50
+# ss = a[0]+a[4] - 50
+
+# jc = a[1] + a[3] - 50
+# js = a[1] + a[4] - 50
+
+# hc = a[2] + a[3] - 50
+# hs = a[2] + a[4] - 50
+# b.append(sc)
+# b.append(ss)
+# b.append(jc)
+# b.append(js)
+# b.append(hc)
+# b.append(hs)
+
+# print(min(b))
+
+#baekjoon11365 !밀비 급일!
+
+
+
+ 
