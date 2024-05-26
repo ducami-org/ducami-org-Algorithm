be85d2ba22da2d2e138aac0824be914aff782dbb
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-05-26 12:48:08 +0900

    codeup1281 수는 더하고 짝수는 뺴고

--- codeup1280.py
+++ codeup1280.py
@@ -1,10 +1,13 @@
 n,m = map(int,input().split())
 sum = 0
 for i in range(n,m+1):
-    if i%2==1:
-        sum+=i
-        print(f"+{i}",end="")
-    else:
+    if i%2==1 and i==n:
+        sum += i
+        print(f"{i}",end='')
+    elif i%2==1:
+        sum += i
+        print(f"+{i}",end='')
+    elif i%2==0:
         sum-=i
         print(f"-{i}",end='')
 
