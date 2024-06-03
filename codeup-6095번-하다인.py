5da777d102ccae19c54714e4cbe945e3e95a41c8
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-06-03 18:25:56 +0900

    codeup-6095번-하다인

--- codeup-6095번-하다인.py
+++ codeup-6095번-하다인.py
@@ -1,1 +1,15 @@
-a=int(input())
+a=[]
+for i in range(20):
+    a.append([])
+    for j in range(20):
+        a[i].append(0)
+b=int(input())
+
+for i in range(b):
+    x,y=map(int,input().split())
+    a[x][y]=1
+
+for i in range(1,20):
+    for j in range(1,20):
+        print(a[i][j],end=' ')
+    print()
