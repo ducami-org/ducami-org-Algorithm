6693bca20ca02648f4fae5ecaae9815f1c01d52e
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-06-03 20:18:15 +0900

    codeup-1289번-하다인

---
+++ codeup-1288번-하다인.py
@@ -0,0 +1,4 @@
+a,b=map(int,input().split())
+for i in range(a,1,-1):
+    for j in range():
+        print()
---
+++ codeup-1289번-하다인.py
@@ -0,0 +1,8 @@
+a,b=map(int,input().split())
+c,d=map(int,input().split())
+e,f=map(int,input().split())
+avg1=a*b
+avg2=c*d
+avg3=e*f
+g=[avg1,avg2,avg3]
+print(max(g))
