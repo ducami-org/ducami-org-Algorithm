81fe9fb64e7076f87a52444e294359aa1cc08c43
donggyengho <ukat7@gmail.com>
2024-06-05 18:35:11 +0900

    codeup 1256, 1257, 1258, 1259, 1260 번 동경호

---
+++ codeup1259.py
@@ -0,0 +1,9 @@
+
+n = int(input())
+sum_of_num = 0
+
+for i in range(1, n+1):
+    if(i % 2 == 0):
+        sum_of_num += i
+
+print(sum_of_num)
---
+++ codeup1260.py
@@ -0,0 +1,10 @@
+sum_of_num = 0
+a, b = input().split()
+a = int(a)
+b = int(b)
+​
+for i in range(a, b+1):
+    if(i % 3 == 0):
+        sum_of_num += i
+​
+print(sum_of_num)
