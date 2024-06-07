df3635b1c1f2fdf4e7873e87b45ed955e6b2bd7c
kimseonghan <imground11@gmail.com>
2024-06-07 20:45:09 +0900

    codeup_1562

---
+++ codeup_1562.py
@@ -0,0 +1,9 @@
+def sum(num, num1):
+    if num < num1:
+        return num
+    elif num > num1:
+        return num1
+    else:
+        return num
+a, b = map(int, input().split())
+print(sum(a,b))
