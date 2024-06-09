a0216d2b15e813400d6c5cc9827b5850bdedfeb0
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-06-10 08:32:47 +0900

    baekjoon2839 설탕배달

---
+++ baekjoon2839.py
@@ -0,0 +1,12 @@
+# n = int(input())
+# cnt = 0
+
+# while n % 5 != 0:
+#     if n % 5 != 0:
+#         n -= 3
+#         cnt += 1
+# if n < 0:
+#     print(-1)
+# elif n % 5 == 0:
+#     print((n//5)+cnt)
+
