733810163e2e503abff92132a1c15dc936e75f75
seo <qazcjik@gmali.com>
2024-06-10 21:17:04 +0900

    codeup-1269,1270,1283,1284,1712/ baekjoon-bottle return

--- codeup-1284 : 암호 해독.py
+++ codeup-1284 : 암호 해독.py
@@ -6,10 +6,10 @@ for i in range(2, n):
     if n % i == 0:
         list.append(i)
 
-if len(list) == 2:
+if len(list) >= 2:
     if 4 in list:  
         print("wrong number")
-    else:
+    else:                              
         for i in list:
             print(i,end=" ")
 
