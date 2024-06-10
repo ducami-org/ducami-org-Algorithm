aa3edaacc044db3ec42c1660f7facea81e8f94e9
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-06-11 08:07:57 +0900

    baekjoon-1920 수 찾기

--- baekjoon-1920.py
+++ baekjoon-1920.py
@@ -1,11 +1,8 @@
-a =int(input())
-for i in range(a):
-    b = list(map(int,input().split()))
-    c = list(map(int,input().split()))
-    if(b[0]+b[1]*2+b[2]*3+b[3]*3+b[4]*4+b[5]*10 == c[0]+c[1]*2+c[2]*2+c[3]*2+c[4]*3+c[5]*5+c[6]*10):
-        print(f'Battle {i+1}: No victor on this battle field')
-    elif(b[0]+b[1]*2+b[2]*3+b[3]*3+b[4]*4+b[5]*10 > c[0]+c[1]*2+c[2]*2+c[3]*2+c[4]*3+c[5]*5+c[6]*10):
-        print(f'Battle {i+1}: Good triumphs over Evil')
+a = input()
+b = set(input().split())
+c = input()
+for i in input().split():
+    if(i in b):
+        print(1)
     else:
-        print(f'Battle {i+1}: Evil eradicates all trace of Good')
-        
+        print(0)
