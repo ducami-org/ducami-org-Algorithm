1c761700845716b111c7808655b0a3049451521a
kimseonghan <imground11@gmail.com>
2024-06-11 20:23:12 +0900

    codeup_1571

---
+++ codeup_1571.py
@@ -0,0 +1,17 @@
+def sum(q, a, b):
+    temp = 0
+    sum = 0
+    for i in range(0, q):
+        if a[i] > b:
+            if temp == 0:
+                temp = i+1
+        elif a[i]<=b:
+            sum+=1
+    if sum == q:
+        return q+1
+    else:
+        return temp
+q = int(input())
+a = list(map(int, input().split()))
+b = int(input())
+print(sum(q, a, b))
