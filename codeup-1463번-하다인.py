5a21275c88a161c9db11f19482f36633ccc1b73c
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-06-17 19:25:10 +0900

    beakjoon-10811번-하다인

--- codeup-1463번-하다인.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-for i in range(1,n+1):
-    for j in range(0,n):
-        print(n*j-i, end=' ')
-    print()
--- codeup-6096번-하다인.py
+++ codeup-6096번-하다인.py
@@ -1,15 +1,29 @@
-a=[]
-for i in range(20):
-    a.append([])
-    for j in range(20):
-        a[i].append(0)
-b=int(input())
+d=[]
+for i in range(20) :
+    d.append([])
+    for j in range(20) :
+        d[i].append(0)
 
-for i in range(b):
+for i in range(19) :
+    a = input().split()
+    for j in range(19) :
+        d[i+1][j+1] = a[j]
+
+n = int(input())
+for i in range(n) :
     x,y=map(int,input().split())
-    a[x][y]=1
 
-for i in range(1,20):
-    for j in range(1,20):
-        print(a[i][j],end=' ')
+    for j in range(1, 20) :
+        if d[j][y]==0 :
+            d[j][y]=1
+        else :
+            d[j][y]=0
+
+        if d[x][j]==0 :
+            d[x][j]=1
+        else : d[x][j]=0
+
+for i in range(1, 20) :
+    for j in range(1, 20) :
+        print(d[i][j], end=' ')
     print()
