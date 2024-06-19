1e60bbddb3f38529e67843891c8245d8aeed6803
kimseonghan <imground11@gmail.com>
2024-06-19 17:48:02 +0900

    baekjoon 1110

---
+++ 백준 1110.py
@@ -0,0 +1,13 @@
+n = input()
+num = n
+cnt = 0
+
+while 1:
+    if len(num) == 1:
+        num = "0"+num
+    plus = str(int(num[0]) + int(num[1]))
+    num = num[-1] + plus[-1]
+    cnt +=1
+    if num == n:
+        print(cnt)
+        break
