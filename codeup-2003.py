467ba8f63ccae87879d3f31d1e6566dce472542e
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-06-19 13:27:06 +0900

    codeup-2003 아이콘 확대

---
+++ codeup-2003.py
@@ -0,0 +1,16 @@
+a = int(input())
+for i in range(a):
+    print('*'*a,end="")
+    print('x'*a,end="")
+    print('*'*a,end="")
+    print()
+for i in range(a):
+    print(' '*a,end="")
+    print('x'*a,end="")
+    print('x'*a,end="")
+    print()
+for i in range(a):
+    print('*'*a,end="")
+    print(' '*a,end="")
+    print('*'*a,end="")
+    print()
