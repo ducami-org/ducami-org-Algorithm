61b700a1632fbebc541497c72c4f50392e2d058a
seo <qazcjik@gmali.com>
2024-06-20 19:19:40 +0900

    baekjoon-10798(세로읽기)

---
+++ baekjoon-10798(세로읽기).py
@@ -0,0 +1,10 @@
+arr=[]
+for i in range(5):
+    arr.append(input())
+
+for i in range(15):
+    for j in range(5):
+        try:
+            print(arr[j][i],end="")
+        except IndexError:
+            pass
