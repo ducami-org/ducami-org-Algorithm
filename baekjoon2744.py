1026ce9dfa1c95d88aef2f91ac75cd33f2388d2f
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-06-21 21:46:44 +0900

    codeup6039

--- baekjoon2744.py
+++ baekjoon2744.py
@@ -1,6 +1,23 @@
-n = input()
-for i in range(len(n)):
-    if 97 <= ord(n[i]) <= 122:
-        print(n[i].upper(),end='')
-    else:
-        print(n[i].lower(),end='')
+# n = input()
+# for i in range(len(n)):
+#     if 97 <= ord(n[i]) <= 122:
+#         print(n[i].upper(),end='')
+#     else:
+#         print(n[i].lower(),end='')
+
+
+n = int(input())
+result = []
+max_y = []
+max_m = []
+max_d = []
+name = []
+for i in range(n):
+    n,d,m,y = input().split()
+    max_y.append(int(y))
+    max_m.append(int(m))
+    max_d.append(int(d))
+    name.append(n)
+if max_y.count(max(max_y)) != 0:
+    
+            
