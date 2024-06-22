98eaf471dd305f005d375c1d48a7a4eaddf93aaa
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-06-22 16:03:26 +0900

    baekjoon-31907 GIST 찍기

---
+++ baekjoon-31907.py
@@ -0,0 +1,13 @@
+a = int(input())
+for i in range(a):
+    print('G'*a,end="")
+    print('.'*(a*3))
+for i in range(a):
+    print('.'*a,end="")
+    print('I'*a,end="")
+    print('.'*a,end="")
+    print('T'*a)
+for i in range(a):
+    print('.'*(a*2),end="")
+    print('S'*a,end="")
+    print('.'*a)
