9fb73ff26e4d4839bb975584713e583e5a3c7e58
kimseonghan <imground11@gmail.com>
2024-06-23 19:08:28 +0900

    baekjoon 2941

--- 백준 2941.py
+++ 백준 2941.py
@@ -1,27 +1,50 @@
+d = []
 a = input()
-count=0
+count = 0
+zz = 0
 for i in range(len(a)):
-    if a[i] == "c":
-        if a[i:+i+2] == "c=":
+    if zz == 0:
+        if a[i] == "c" :
+            if a[i+1] == "=" or a[i+1] == "-":
+                count+=1
+                zz = 1
+            else:
+                count+=1
+        elif a[i] == "d":
+            if a[i+2] == "=" and a[i+1] == "z":
+                count+=1
+                zz = 2
+            elif  a[i+1] == "-":
+                count+=1
+                zz = 1
+            else:
+                count+=1
+        elif a[i] == "l":
+            if a[i+1] == "j":
+                count+=1
+                zz = 1
+            else:
+                count+=1
+        elif a[i] == "n":
+            if a[i+1] == "j":
+                count+=1
+                zz = 1
+            else:
+                count+=1
+        elif a[i] == "s":
+            if a[i+1] == "=":
+                count+=1
+                zz = 1
+            else:
+                count+=1
+        elif a[i] == "z":
+            if a[i+1] == "=":
+                count +=1
+                zz = 1
+            else:
+                count+=1
+        else:
             count+=1
-        elif a[i:+i+2] == "c-":
-            count+=1
-    elif a[i] == "d":
-        if a[i:i+3] == "dz=":
-            count+=1
-        elif a[i:i+2] == "d-":
-            count+=1
-    elif a[i] == "l":
-        if a[i:i+2] == "lj":
-            count+=1
-    elif a[i] == "n":
-        if a[i:i+2] == "nj":
-            count+=1
-    elif a[i] == "s":
-        if a[i:i+2] == "s=":
-            count+=1
-    elif a[i]== "z":
-        if a[i:i+2] == "z=":
-            count+=1
+    else:
+        zz-=1
 print(count)
-print(a[0:2])
