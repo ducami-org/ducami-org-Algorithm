03abc1095ed94f8f31d2b81c1d0e62e4094178c8
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-06-24 10:16:12 +0900

    baekjoon2566 최댓값

---
+++ baekjoon2566.py
@@ -0,0 +1,17 @@
+max_n = 0
+y = 0
+x = 0
+max_x = 0
+max_y = 0
+for i in range(9):
+    if i != 0:
+       y+= 1
+    for j in range(1):
+        a = list(map(int,input().split()))
+        if max(a) > max_n:
+            max_n = max(a)
+            x = a.index(max_n)
+            max_x = x
+            max_y = y            
+print(max_n)
+print(max_y+1,max_x+1)
