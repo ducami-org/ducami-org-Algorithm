2e5601ba72e58f698ed9328dbfaa685241fa2c39
dgsw05 <dgsw05@dgsw05ui-MacBookAir.local>
2024-06-24 19:36:17 +0900

    codeup-1230번-하다인

---
+++ codeup-1230번-하다인.py
@@ -0,0 +1,11 @@
+a,b,c=map(int,input().split())
+if(a <= 170):
+    print('CRASH ' + str(a))
+elif(b <= 170):
+    print('CRASH ' + str(b))
+elif(c <= 170):
+    print('CRASH ' + str(c))
+else:
+    print('PASS')
+
+        
