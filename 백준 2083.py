cf91336a0b0de3245a89e9da479da505ca8a4bf5
kimseonghan <imground11@gmail.com>
2024-06-25 20:18:07 +0900

    baekjoon 2083

---
+++ 백준 2083.py
@@ -0,0 +1,11 @@
+while True:
+    n,a,w = input().split()
+    a = int(a)
+    w = int(w)
+    if n == "#" and a == 0 and w == 0:
+        break
+    else:
+        if a>17 or w>=80:
+            print(f"{n} Senior")
+        else:
+            print(f"{n} Junior")
