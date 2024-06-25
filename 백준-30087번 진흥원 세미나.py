3098e56fe06f3062ed7b2f7e4a49f66b7b9d7102
tlsalsco <tlsalsco@dgsw.hs.kr>
2024-06-25 15:24:52 +0900

    백준-30084번 진흥원 세미나

---
+++ 백준-30087번 진흥원 세미나.py
@@ -0,0 +1,6 @@
+a = {'Algorithm':204, 'DataAnalysis':207, 'ArtificialIntelligence':302, 'CyberSecurity':'B101','Network':303,'Startup':501, 'TestStrategy':105}
+N = int(input())
+
+for i in range(N):
+    b = input()
+    print(a.get(b))
