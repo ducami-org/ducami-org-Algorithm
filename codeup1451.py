203b2b685b70b1c7485412bd3b1e7aadf236ea8d
Yongjin081002 <163402661+Yongjin081002@users.noreply.github.com>
2024-06-25 15:46:37 +0900

    baekjoon9316

--- codeup1451.py
+++ codeup1451.py
@@ -1,7 +1,15 @@
+# n = int(input())
+# a = []
+# for i in range(n):
+#     a.append(int(input()))
+# a.sort()
+# for i in a:
+#     print(i)
+
+def hamsu(n):
+    if n == 0:
+        return 0
+    return n + hamsu(n-1)
+
 n = int(input())
-a = []
-for i in range(n):
-    a.append(int(input()))
-a.sort()
-for i in a:
-    print(i)
+print(hamsu(n))
