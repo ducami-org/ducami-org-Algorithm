6e28c5c34ccacbeade71f4088c6e830e98dbbb4d
dgsw43 <dgsw43@dgsw43ui-MacBookAir.local>
2024-06-26 11:42:07 +0900

    baekjoon-28453 Previous Level

---
+++ baekjoon-28453.py
@@ -0,0 +1,11 @@
+a = input()
+b = list(map(int,input().split()))
+for i in b:
+    if(i>=300):
+        print(1,end=" ")
+    elif(i>=275):
+        print(2, end=" ")
+    elif(i>=250):
+        print(3,end=" ")
+    else:
+        print(4,end=" ")
